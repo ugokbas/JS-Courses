aaa3a9079c5953d1018e8a9975b279ee33e736a9
Ukbe Gökbaş <ugokbas@gmail.com>
2021-05-25 16:06:30 +0300

    new-version

---
+++ chapter4_dom/jdhsjhddjshd/app.js
@@ -0,0 +1,10 @@
+const paragraf = document.getElementById("par");
+paragraf.style.backgroundColor = "black";
+paragraf.style.color = "white";
+
+const buton = document.getElementById("btn");
+buton.style.width = "100px";
+buton.style.height = "30px";
+buton.style.backgroundColor = "orange";
+buton.style.color = "black";
+buton.innerHTML = "Ara";
