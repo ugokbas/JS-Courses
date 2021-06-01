505a6ca8ad347fb7f6ab24fda0e3add0e4241a3d
Ukbe Gökbaş <ugokbas@gmail.com>
2021-06-01 14:47:31 +0300

    cylinder-ex

---
+++ chapter4_dom/app.js
@@ -0,0 +1,7 @@
+function calculate(){
+    const r = document.getElementById("r").value;
+    const h = document.querySelector(".h").value; // ! (.class_name) for class name in querySelector
+                                             // ! (#id_name) for id name in querySelector
+    const result = Math.PI * r * r * h;
+    document.querySelector(".volume").innerHTML = result.toFixed(2);                                          
+}
