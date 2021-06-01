b084304bdc5b2c8400435b4fe4ccd2ca6dc02a88
Ukbe Gökbaş <ugokbas@gmail.com>
2021-06-01 16:47:21 +0300

    edit

--- chapter4_dom/example1_calculateVolume/app.js
+++ chapter4_dom/example1_calculateVolume/app.js
@@ -1,7 +1,11 @@
 function calculate(){
     const r = document.getElementById("r").value;
     const h = document.querySelector(".h").value; // ! (.class_name) for class name in querySelector
-                                             // ! (#id_name) for id name in querySelector
+                                                  // ! (#id_name) for id name in querySelector
     const result = Math.PI * r * r * h;
     document.querySelector(".volume").innerHTML = result.toFixed(2);                                          
 }
+
+const button = document.querySelector(".btn");
+button.style.color = "red";
+button.style.backgroundColor = "cream";
