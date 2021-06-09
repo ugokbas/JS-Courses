6e5279fce022a764f6969a5b5b69a9d95fe712b7
Ukbe Gökbaş <ugokbas@gmail.com>
2021-06-09 23:08:36 +0300

    end-of-the-day

--- chapter4_dom/example3_lightABulb/app.js
+++ chapter4_dom/example3_lightABulb/app.js
@@ -13,3 +13,22 @@ document.querySelector(".image").addEventListener("mouseover", () => {
 document.querySelector(".image").addEventListener("mouseout", () => {
     document.querySelector(".image").src = "./img/light-off.jpg";
 })
+
+document.querySelector(".textbox").addEventListener("keyup", () => {
+    const checkbox = document.querySelector(".checkbox");
+    const textbox = document.querySelector(".textbox");
+
+    if(checkbox.checked){
+        textbox.value = textbox.value.toUpperCase();
+    } else{
+        textbox.value = textbox.value.toLowerCase();
+    }
+})
+
+const afterDiv = document.querySelector(".div-input")
+const h1 = document.createElement("h1");
+const text = document.createTextNode("Programming Languages");
+h1.appendChild(text);
+afterDiv.after(h1);
+h1.style.marginTop = "20px";
+h1.style.color = "red";
