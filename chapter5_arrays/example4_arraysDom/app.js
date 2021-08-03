9823cfac5356a8019d4cddf77225e8d480ee027a
Ukbe Gökbaş <ugokbas@gmail.com>
2021-08-03 22:41:50 +0300

    endOfMiniProject

--- chapter5_arrays/example4_arraysDom/app.js
+++ chapter5_arrays/example4_arraysDom/app.js
@@ -1,4 +1,4 @@
-const wages = [4000, 5000, 3500, 4200, 2850];
+const wages = [4000, 5000, 3500, 4200, 2850, 7500, 3000];
 
 const list = document.createElement("ul");
 list.className = "list";
@@ -16,16 +16,25 @@ const addArrayToList = function(){
     }
 }
 
+const updateExplanation = function(arr) {
+    const explanation = document.querySelector(".explanation");
+    const newWages = arr.join(" ");
+    explanation.innerHTML = `Wages: ${newWages} <br>
+    Total Wage: ${arr.reduce((x,y) => x + y, 0)}`;
+}
+
 addArrayToList();
+updateExplanation(wages);
 
 document.querySelector(".add").onclick = function(){
-    let add = document.querySelector(".input-list");
+    const add = document.querySelector(".input-list");
     if(!add.value){
         alert("You didn't enter a wage")
     }else{
         wages.push(Number(add.value));
         addToList(add.value);
         add.value = "";
+        updateExplanation(wages);
     } 
 }
 
@@ -36,6 +45,30 @@ document.querySelector(".delete").onclick = function(){
         wages.pop();
         const list = document.querySelector(".list");
         list.removeChild(list.lastElementChild);
+        updateExplanation(wages);
     }  
 }
 
+document.querySelector(".search").onclick = function(){
+    const add = document.querySelector(".input-list");
+    if(!add.value){
+        alert("Enter the top wage limit.");
+    }else{
+        const filtered = wages.filter((m) => m <= add.value);
+        if(filtered.length == 0) {
+            alert("Cannot find any wages!");
+        } else{
+            updateExplanation(filtered);
+        }
+    }
+    add.value = "";
+}
+
+document.querySelector(".input-list").onkeydown = function(e){
+    if(e.key === "Enter"){
+        document.querySelector(".add").onclick();
+    } else if(e.key === "Del"){
+        document.querySelector("delete").onclick();
+    }
+}
+    
