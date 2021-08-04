6961359d4792bcba326d36bc67a3724fe4551c10
Ukbe Gökbaş <ugokbas@gmail.com>
2021-08-04 22:56:02 +0300

    objects

---
+++ chapter6_objects/objects.js
@@ -0,0 +1,18 @@
+const human = {
+    name: "Can",
+    surname: "Yilmaz",
+    age: 30,
+    job: "Software Developer",
+    languages: ["Java", "JS", "C#", "SQL"]
+}
+
+console.log(human);
+
+//.(Dot) Notation
+console.log(human.job);
+
+//
+console.log(human["name"]);
+
+const words = "My name is ";
+console.log(`${words} ${human.name}`);
